a7beef18e0b3671b305e77017886cc686daec7db
Natalie98x <ntorre28@illinois.edu>
2020-05-07 14:05:23 -0700

    Update 5.7.20

---
+++ scrollFlipBook_sections/javaScriptCode/p5_test.js
@@ -0,0 +1,43 @@
+var pathEls;
+
+function setup(){
+
+}
+
+function draw(){
+
+}
+
+let myPaths= document.querySelectorAll('Path')
+
+function setup(){
+anime({
+  targets: myPaths [0],
+    translateX: 100
+    
+    
+})
+
+anime({
+  targets: myPaths [1],
+    translateX: -100
+    
+    
+})
+anime({
+  targets: myPaths [2],
+    translateX: -100
+    
+    
+})
+}
+
+function mousePressed(){
+anime({
+  targets: myPaths [3],
+    translateX: -100
+    
+    
+})
+}
+
